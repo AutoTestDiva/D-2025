acef5a9e6372c415aacdb6b037b492fca2d73168
Nataliia <pnata1978@gmail.com>
2025-02-14 16:15:15 +0100

    start-1

--- src/test/java/org/nat/qa2025/OpenSiteTest.java
+++ src/test/java/org/nat/qa2025/OpenSiteTest.java
@@ -1,5 +1,20 @@
 package org.nat.qa2025;
 
-public class OpenSiteTest {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
+public class OpenSiteTest {
+    WebDriver driver;
+@BeforeMethod
+    public void setUp(){
+    driver = new ChromeDriver();
+    //driver.get("https://www.google.com/");
+    driver.navigate().to("https://www.google.com/");
+}
+@Test
+    public void openGoogleTest(){
+    System.out.println("Site opened");
+}
 }
