503e3c74acdd9a472a7558a1015fc128e801f7ab
Nataliia <pnata1978@gmail.com>
2025-02-15 18:16:28 +0100

    findElementTest-1

---
+++ src/test/java/org/nat/qa2025/FindElementTests.java
@@ -0,0 +1,23 @@
+package org.nat.qa2025;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FindElementTests {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.get("https://ilcarro.web.app");
+        //max window
+        driver.manage().window().maximize();
+
+    }
+    @Test
+    public void findElementTest(){
+
+    }
+}
