ebc3d4a1f7c7baf41e12e41c234ec77fca0d3148
Nataliia <pnata1978@gmail.com>
2025-02-15 18:31:37 +0100

    findElementTest-2

--- src/test/java/org/nat/qa2025/FindElementTests.java
+++ src/test/java/org/nat/qa2025/FindElementTests.java
@@ -1,23 +1,38 @@
 package org.nat.qa2025;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class FindElementTests {
     WebDriver driver;
 
     @BeforeMethod
-    public void setUp(){
+    public void setUp() {
         driver = new ChromeDriver();
         driver.get("https://ilcarro.web.app");
         //max window
         driver.manage().window().maximize();
+        //timeout
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
     }
-    @Test
-    public void findElementTest(){
 
+    @Test
+    public void findElementTest() {
+        WebElement element = driver.findElement(By.tagName("h1"));
+        System.out.println(element.getText());
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
     }
 }
